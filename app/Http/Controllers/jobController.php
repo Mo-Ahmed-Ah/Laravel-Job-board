d53c46d278834706e895e663b462198103f5d9ed
Ahmed Mohamed <ahmed0033ahmed.mo@gmail.com>
2025-08-15 11:05:07 +0300

    Belad Templating Engine Module

--- app/Http/Controllers/jobController.php
+++ app/Http/Controllers/jobController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Jop;
 
-class obController extends Controller
+class JobController extends Controller
 {
     public function index()
     {
